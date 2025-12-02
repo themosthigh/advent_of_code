8e82cab2529b114251e00a1d8291c083be72ddc7
TheMostHigh <tallestlegacy@gmail.com>
2025-12-02 14:48:09 +0300

    2025 1.2 WIP unsolved

--- 2025/day_01/src/jay.js
+++ 2025/day_01/src/jay.js
@@ -4,21 +4,29 @@ const input = fs.readFileSync("../input.txt").toString();
 
 let dial = 50;
 let acc = 0;
+let acc_abs = 0;
 
 for (line of input.split("\n")) {
+  const previous = dial;
+
   if (!line) continue;
   const operator = line[0];
   const value = parseInt(line.split("").slice(1).join(""));
 
   if (operator == "L") {
-    dial = (100 + dial - value) % 100;
+    dial = dial - value;
   } else if (operator == "R") {
-    dial = (dial + value) % 100;
+    dial = dial + value;
   }
 
   if (dial % 100 === 0) {
     acc++;
   }
+
+  const diff = Math.abs(dial - previous);
+  const diff_base = Math.floor(diff / 100);
+  console.log({ previous, dial, diff_base });
+  acc_abs += diff_base;
 }
 
-console.log({ acc });
+console.log({ acc, dial, acc_abs, res: acc + acc_abs });
