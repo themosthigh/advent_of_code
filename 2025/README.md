8e0a6b15e7b3df0847a33702561f4028233ce6e6
TheMostHigh <tallestlegacy@gmail.com>
2025-12-07 10:20:43 +0300

    2025 2.1 WIP unsolved

---
+++ 2025/README.md
@@ -0,0 +1,47 @@
+Checklist
+
+- [ ] Day 1
+  - [x] Part 1
+  - [ ] Part 2
+- [ ] Day 2
+  - [ ] Part 1
+  - [ ] Part 2
+- [ ] Day 3
+  - [ ] Part 1
+  - [ ] Part 2
+- [ ] Day 4
+  - [ ] Part 1
+  - [ ] Part 2
+- [ ] Day 5
+  - [ ] Part 1
+  - [ ] Part 2
+- [ ] Day 6
+  - [ ] Part 1
+  - [ ] Part 2
+- [ ] Day 7
+  - [ ] Part 1
+  - [ ] Part 2
+- [ ] Day 8
+  - [ ] Part 1
+  - [ ] Part 2
+- [ ] Day 9
+  - [ ] Part 1
+  - [ ] Part 2
+- [ ] Day 10
+  - [ ] Part 1
+  - [ ] Part 2
+- [ ] Day 11
+  - [ ] Part 1
+  - [ ] Part 2
+- [ ] Day 12
+  - [ ] Part 1
+  - [ ] Part 2
+- [ ] Day 13
+  - [ ] Part 1
+  - [ ] Part 2
+- [ ] Day 15
+  - [ ] Part 1
+  - [ ] Part 2
+- [ ] Day 15
+  - [ ] Part 1
+  - [ ] Part 2
---
+++ 2025/day_02/src/main.rs
@@ -0,0 +1,48 @@
+const INPUT: &str = include_str!("../input.txt");
+
+fn main() {
+    let mut acc = 0;
+    for range in INPUT.trim().split(",") {
+        // make all the inputs into an array
+        let range_arr: Vec<u64> = range.split("-").map(|e| e.parse().unwrap()).collect();
+
+        for num in range_arr[0]..range_arr[1] + 1 {
+            if has_repeating_digits(num) {
+                acc = acc + num;
+            }
+        }
+    }
+    println!("Acc {acc}");
+}
+
+fn has_repeating_digits(num: u64) -> bool {
+    let num_str: String = num.to_string();
+    let str_len = num_str.len();
+
+    // only even length digits count
+    if str_len % 2 != 0 {
+        return false;
+    }
+
+    for pairs in 1..str_len / 2 + 1 {
+        let splits: Vec<&str> = num_str
+            .as_bytes()
+            .chunks(pairs)
+            .map(|chunk| std::str::from_utf8(chunk).unwrap())
+            .collect();
+
+        let first_num = splits[0];
+
+        let matching_res: Vec<&&str> = splits.iter().filter(|num| **num == first_num).collect();
+        if matching_res.len() == splits.len() && splits.len() == 2 {
+            println!(
+                "{num_str} :: {:?} len({})",
+                matching_res,
+                matching_res.len()
+            );
+            return true;
+        }
+    }
+
+    return false;
+}
